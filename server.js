a63710a9e1c4d45e4f66fcdb7e5aec0e6cd9cca1
Crash-Zeus <jonas.bertin49@gmail.com>
2020-02-26 22:14:30 +0100

    Begining use mongoDB

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const HOST = '0.0.0.0';
 const PORT = 8080;
- 
+
+const schema  = mongoose.Schema({
+    title : String,
+    type : String,
+    number : Number
+})
+module.exports = mongoose.model('book', schema);
+
+const Books = require('./book');
+mongoose.connect('mongodb://localhost:8081/book', {useNewUrlParser: true});
 var app = express();
 var Router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-Router.route('/').post(function(req, res){
+Router.route('/login').post(function(req,res) {
+
+});
+
+Router.route('/save').post(function(req, res){
 	res.json({params: req.body});
 });
 
