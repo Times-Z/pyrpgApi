209ebb3aa3a1bcf691b9450ada1bc1339a3bb370
Jonas <j.bertin@maxicoffee.com>
2020-02-27 09:00:42 +0100

    trying add mongoDB db

--- server.js
+++ server.js
@@ -2,10 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const HOST = '0.0.0.0';
 const PORT = 8080;
- 
+
+const schema = mongoose.Schema({
+	title : String,
+	type : String,
+	price : Number
+})
+module.exports = mongoose.model('books', schema);
+const Book = require('./books');
+mongoose.connect('mongodb://localhost:9050/Book', {useNewUrlParser: true});
 var app = express();
 var Router = express.Router();
 
