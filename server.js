943de8a94940390de1eb4cc8b67b531b7e1d95a4
Crash-Zeus <jonas.bertin49@gmail.com>
2020-02-27 21:58:52 +0100

    rm mongoose add sqlite3 route /login work with postman json

--- server.js
+++ server.js
@@ -1,26 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Book = require('./books');
-
-mongoose.connect('mongodb://localhost:9081/Book', {useNewUrlParser: true, useUnifiedTopology: true});
-
 const HOST = '0.0.0.0';
-// const HOST = '127.0.0.1';
 const PORT = 8080;
-// const PORT = 9080;
+const argon2 = require('argon2');
+const sq = require('sqlite3');
+sq.verbose();
 
+// Postman json exemple for post route
+// {
+//     "username": "testuser",
+//     "password": "123456"
+// }
+
+var db = new sq.Database(__dirname + '/users.db3');
 var app = express();
 var Router = express.Router();
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-Router.route('/login').post(function(req,res) {
 
+Router.route('/login').post(function (req, res) {
+	var username = req.body.username
+	var password = req.body.password
+	db.each("SELECT * FROM users WHERE user_name='" + username + "'", async function (err, user) {
+			if (user != '') {
+				if (await argon2.verify(user.user_pass, password)) {
+					var user = user;
+					res.json({
+						"DB user" : user,
+					});
+					console.log(res.statusCode);
+				} else {
+					res.sendStatus(401);
+					console.log(res.statusCode);
+				}
+			} else {
+				res.sendStatus(401);
+				console.log(err);
+			}
+	});
 });
 
-Router.route('/save').post(function(req, res){
-	res.json({params: req.body});
-	console.log(req.body);
+Router.route('/save').post(function (req, res) {
+	res.json({
+		method: req.method,
+		"POST params": req.body,
+		ip: req.ip,
+		code: res.statusCode,
+		message: req.statusMessage
+	});
 });
 
 app.use(Router);
