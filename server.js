c0301f029a4646353ff0905fe054f7fcfe0299a4
Crash-Zeus <jonas.bertin49@gmail.com>
2020-02-29 14:05:36 +0100

    add route signup && return code

--- server.js
+++ server.js
@@ -20,24 +20,45 @@ var Router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+Router.route('/signup').post(async function (req, res) {
+	var username = req.body.username;
+	var email = req.body.email;
+	var rawpass = req.body.password;
+	var pass = await argon2.hash(rawpass);
+	db.get("SELECT user_id FROM users WHERE user_mail = '"+ email + "'", function (err, user) {
+		if (user == null) {
+			db.exec("INSERT INTO users (user_name, user_pass, user_mail) VALUES ('"+username+"','"+pass+"','"+email+"')");
+			res.json({
+				"code": res.statusCode,
+				"message": req.statusMessage
+			});
+		} else {
+			res.status(400).send('Email as already exist in DB');
+			log('IP : ' + req.ip + ' - return code : ' + res.statusCode + ' - on url ' + req.url + ' - with method ' + req.method, 'logs/server.log');
+		}
+	});
+});
+
 Router.route('/login').post(function (req, res) {
-	var username = req.body.username
-	var password = req.body.password
+	var username = req.body.username;
+	var password = req.body.password;
 	db.each("SELECT * FROM users WHERE user_name='" + username + "'", async function (err, user) {
-			if (user != '') {
-				if (await argon2.verify(user.user_pass, password)) {
-					var user = user;
-					res.json({
-						"DB user" : user,
-					});
-				} else {
-					res.sendStatus(401);
-					log('IP : ' + req.ip + ' Return code : ' + res.statusCode + ' On url ' + req.url + ' with method ' + req.method, 'logs/server.log');
-				}
+		if (user != '') {
+			if (await argon2.verify(user.user_pass, password)) {
+				var user = user;
+				res.json({
+					"code": res.statusCode,
+					"message": req.statusMessage,
+					"DB user": user,
+				});
 			} else {
 				res.sendStatus(401);
-				log('IP : ' +req.ip+ ' Return code : ' + res.statusCode, 'logs/server.log');
+				log('IP : ' + req.ip + ' - return code : ' + res.statusCode + ' - on url ' + req.url + ' - with method ' + req.method, 'logs/server.log');
 			}
+		} else {
+			res.sendStatus(401);
+			log('IP : ' + req.ip + ' - return code : ' + res.statusCode + ' - on url ' + req.url + ' - with method ' + req.method, 'logs/server.log');
+		}
 	});
 });
 
