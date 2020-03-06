2e8f467e6ab7546f6fe3e85a7361f0d7baf3ff6d
Crash-Zeus <jonas.bertin49@gmail.com>
2020-03-06 23:24:18 +0100

    add validateToken function

--- functions/functions.js
+++ functions/functions.js
@@ -2,7 +2,23 @@ function updateTimeLog(db, uid) {
     db.exec("UPDATE users SET user_last_log = datetime('now') WHERE user_id = " + uid);
 };
 
+function validateToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.status(403).json({
+            "code": res.statusCode,
+            "message": "Token error"
+        });
+    }
+};
+
 
 module.exports = {
-    updateTimeLog : updateTimeLog
+    updateTimeLog : updateTimeLog,
+    validateToken : validateToken
 };
