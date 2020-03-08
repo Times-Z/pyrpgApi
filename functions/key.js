5904ab70479dc2102d9e98ac8865a786673322ea
Crash-Zeus <jonas.bertin49@gmail.com>
2020-03-08 19:45:21 +0100

    Update key

--- functions/key.js
+++ functions/key.js
@@ -1,5 +1,5 @@
 function getKey() {
-    const key = 'JÉ°3§«¥zÉìTÿóJXÚ¤óµuó·VÞ¾7¦ôh8¦ý®¦j¯ÞSUîhJ²°H¥üäsÞE6xöõø¦ê47¥«ôþÜrsoÃüºÖQBAQãÏçRYc3âÄNEN¨å£¬«Êõ¸7«Õ¬Ý¶6iýBs»º4';
+    const key = '';
     return key
 }
 module.exports = {
